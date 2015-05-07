bfd2859ccc8f9ba6f140714dd4c578c83c1f596e
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2015-05-07 17:45:07 +0900

    refactor

--- spec/v1/notify_incident_spec.rb
+++ spec/v1/notify_incident_spec.rb
@@ -2,87 +2,46 @@ describe 'execute notification' do
   include_context 'default_client_context'
 
   subject do
-    client.notify_incident(incident_id, options)
+    stub.tap { client.notify_incident(incident_id, options) }
   end
-
-  before { stub }
 
   let(:incident_id) { 123 }
 
+  let(:stub) do
+    stub_api_request(
+      organization, api_key,
+      method: :post, path: '/api/v1/notifications',
+      body: { incident_id: incident_id }.merge(expected_options)
+    ).to_return(
+      status: 201
+    )
+  end
+
   context 'without options' do
-    subject { client.notify_incident(incident_id) }
-
-    let(:stub) do
-      stub_api_request(
-        organization, api_key,
-        method: :post, path: '/api/v1/notifications',
-        body: { incident_id: incident_id }
-      ).to_return(
-        status: 201
-      )
-    end
-
-    it do
-      subject
-      expect(stub).to have_been_requested
-    end
+    subject { stub.tap { client.notify_incident(incident_id) } }
+    let(:expected_options) { {} }
+    it { is_expected.to have_been_requested }
   end
 
   context 'with notification text option' do
     let(:options) { { text: text } }
+    let(:expected_options) { { text: text } }
     let(:text) { '至急対応をお願いします。' }
-
-    let(:stub) do
-      stub_api_request(
-        organization, api_key,
-        method: :post, path: '/api/v1/notifications',
-        body: {
-          incident_id: incident_id,
-          text: text
-        }
-      ).to_return(
-        status: 201
-      )
-    end
-
-    it do
-      subject
-      expect(stub).to have_been_requested
-    end
+    it { is_expected.to have_been_requested }
   end
 
   context 'with notification call option' do
     let(:options) { { call: call } }
-
-    let(:stub) do
-      stub_api_request(
-        organization, api_key,
-        method: :post, path: '/api/v1/notifications',
-        body: {
-          incident_id: incident_id,
-          call: call
-        }
-      ).to_return(
-        status: 201
-      )
-    end
+    let(:expected_options) { { call: call } }
 
     context 'given true as call' do
       let(:call) { true }
-
-      it do
-        subject
-        expect(stub).to have_been_requested
-      end
+      it { is_expected.to have_been_requested }
     end
 
     context 'given false as call' do
       let(:call) { false }
-
-      it do
-        subject
-        expect(stub).to have_been_requested
-      end
+      it { is_expected.to have_been_requested }
     end
   end
 end
