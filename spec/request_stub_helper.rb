f1468831e5658175003456cac12f933d521e8818
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2015-05-07 14:50:53 +0900

    use stub request helper

--- spec/request_stub_helper.rb
+++ spec/request_stub_helper.rb
@@ -3,14 +3,21 @@ require 'json'
 module RequestStubHelper
 
   def stub_api_request(organization, api_key, spec)
-    stub_request(spec[:method], "https://#{organization}.reactio.jp#{spec[:path]}")
-      .with(
-        headers: {
-          'Accept' => 'application/json',
-          'Content-Type' => 'application/json',
-          'X-Api-Key' => api_key
-        },
-        body: Hash(spec[:body]).to_json
-      )
+    params = {}
+    params[:headers] = build_headers(api_key)
+    params[:body] = spec[:body].to_json if spec[:body]
+
+    stub_request(
+      spec[:method],
+      "https://#{organization}.reactio.jp#{spec[:path]}"
+    ).with(params)
+  end
+
+  def build_headers(api_key)
+    {
+      'Accept' => 'application/json',
+      'Content-Type' => 'application/json',
+      'X-Api-Key' => api_key
+    }
   end
 end
--- spec/v1/describe_incident_spec.rb
+++ spec/v1/describe_incident_spec.rb
@@ -6,18 +6,18 @@ describe 'describe incident' do
   let(:client) { Reactio::Client.new(api_key: api_key, organization: organization) }
   let(:api_key) { 'api_key_for_organization' }
   let(:organization) { 'your-organization' }
+
   let(:incident_id) { 123 }
 
   before { stub }
 
   let(:stub) do
-    stub_request(:get, "https://#{organization}.reactio.jp/api/v1/incidents/#{incident_id}")
-      .with(:headers => {
-        'Accept' => 'application/json',
-        'Content-Type' => 'application/json',
-        'X-Api-Key' => api_key
-      })
-      .to_return(status: 200)
+    stub_api_request(
+      organization, api_key,
+      method: :get, path: "/api/v1/incidents/#{incident_id}"
+    ).to_return(
+      status: 200
+    )
   end
 
   it do
