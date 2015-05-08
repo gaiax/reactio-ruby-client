b46a451f24421f60ada5b6cb7d45490bcbbd359d
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2015-05-08 15:31:20 +0900

    refactor payload building

--- lib/reactio/service.rb
+++ lib/reactio/service.rb
@@ -13,20 +13,14 @@ module Reactio
     end
 
     def create_incident(name, options = {})
-      body = { name: name }
-      body[:status] = options[:status] if options[:status]
-      body[:detection] = to_option_string(options[:detection]) if options.key?(:detection)
-      body[:cause] = to_option_string(options[:cause]) if options.key?(:cause)
-      body[:cause_supplement] = options[:cause_supplement] if options[:cause_supplement]
-      body[:point] = to_option_string(options[:point]) if options.key?(:point)
-      body[:scale] = to_option_string(options[:scale]) if options.key?(:scale)
-      body[:pend_text] = options[:pend_text] if options[:pend_text]
-      body[:close_text] = options[:close_text] if options[:close_text]
-      body[:topics] = options[:topics] if options[:topics]
-      body[:notification_text] = options[:notification_text] if options[:notification_text]
-      body[:notification_call] = options[:notification_call] if options.key?(:notification_call)
-
-      @api.request(:post, "/api/v1/incidents", body: body)
+      payload = { name: name }.tap do |me|
+        me[:detection] = to_option_string(options.delete(:detection)) if options.key?(:detection)
+        me[:cause] = to_option_string(options.delete(:cause)) if options.key?(:cause)
+        me[:point] = to_option_string(options.delete(:point)) if options.key?(:point)
+        me[:scale] = to_option_string(options.delete(:scale)) if options.key?(:scale)
+      end
+        .merge!(options)
+      @api.request(:post, "/api/v1/incidents", body: payload)
     end
 
     def describe_incident(incident_id)
@@ -34,22 +28,20 @@ module Reactio
     end
 
     def list_incidents(options = {})
-      body = {}
-      body[:from] = options[:from].to_i if options[:from]
-      body[:to] = options[:to].to_i if options[:to]
-      body[:status] = options[:status].to_s if options[:status]
-      body[:page] = options[:page] if options[:page]
-      body[:per_page] = options[:per_page] if options[:per_page]
-
-      @api.request(:get, "/api/v1/incidents", body: body)
+      payload = [].tap do |me|
+        me[:from] = options.delete(:from).to_i if options[:from]
+        me[:to] = options.delete(:to).to_i if options[:to]
+        me[:status] = options.delete(:status).to_s if options[:status]
+      end
+        .merge!(options)
+      @api.request(:get, "/api/v1/incidents", body: payload)
     end
 
     def notify_incident(incident_id, options = {})
-      body = { incident_id: incident_id }
-      body[:text] = options[:text] if options[:text]
-      body[:call] = options[:call] unless options[:call].nil?
-
-      @api.request(:post, "/api/v1/notifications", body: body)
+      @api.request(
+        :post, "/api/v1/notifications",
+        body: { incident_id: incident_id }.merge(options)
+      )
     end
   end
 end
